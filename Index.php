6f74aaf3a3eebef70bd801a8aa39f62a4e611fec
Ca.Tefa <estefaniaalarcon2011@gmail.com>
2021-07-04 23:28:42 -0500

    Se agrega todas las carpetas y archivos que se tienen hasta 04/07

---
+++ Index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/c345d64496.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500&family=Raleway:ital,wght@0,300;1,100&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="Assets/css/Styles.css">
+    <title>Zaiko</title>
+</head>
+<body>
+
+    <!-- Menú de navegación 
+    @author Daniel Puerta Bernal 
+    @email danielpuerta5789@gmail.com-->
+
+    <nav>
+        <label class="logo">Zaiko</label>
+            <ul>
+                <li><a class="active nav-link" href="#Masthead"><b>Inicio</b></a></li>
+                <li><a class="nav-link" href="#We"><b>Nosotros</b></a></li>
+                <li><a class="nav-link" href="#Services"><b>Servicios</b></a></li>
+                <li><a class="nav-link" href="#Contacts"><b>Contactos</b></a></li>
+            </ul>
+    </nav>
+
+    <!-- Fin del menú de navegación -->
+
+    <!-- Masthead/Cabecera
+    @author Estefanía Carmona Alarcón 
+    @email estefaniaalarcon2011@gmail.com-->
+    
+    <header id="Masthead">
+        <div> 
+            <div>
+                <h1 class="Title"><b>Zaiko</b></h1>
+                <h2 class="Slogan"><b>Administrador de inventarios</b></h2>
+                <a href="#" class="Button">Ingresar</a>
+            </div>  
+                <div class="Bead">
+                <img src="Assets/img/Inventary.svg" class="img1">
+            </div>
+            <div class="Bead2"></div>
+        </div>
+    </header>
+    <!-- Fin Masthead -->
+
+    <!-- Us 
+    @author Daniel Puerta Bernal
+    @email danielpuerta5789@gmail.com-->
+
+    <section id="We" class="Section_We">
+        <div class="Container">
+            <div class="Slider-container">
+                <section class="Slider-content"> 
+                    <div>
+                        <h1><b>Nosotros</b></h1>
+                        <div class="Hexagon1">
+                            <div class="Hexagon"></div>
+                        </div>
+                        <div class="Hexagon2">
+                            <div class="Hexagon"></div>
+                        </div>
+                        <div class="Hexagon3">
+                            <div class="Hexagon"></div>
+                        </div>
+                    </div> 
+                    <div class="Text-we">
+                      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae iure, est, laborum saepe animi distinctio repellendus unde quos minima officiis, non modi architecto esse praesentium! Dolore ipsam perspiciatis maiores ullam.</h3> 
+                      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae iure, est, laborum saepe animi distinctio repellendus unde quos minima officiis, non modi architecto esse praesentium! Dolore ipsam perspiciatis maiores ullam.</h3>  
+                    </div>
+                </section>
+            </div>
+        </div>
+    </section>
+    
+    <!-- Fin Us -->
+
+    <script src="Assets/js/Slider.js"></script>
+</body>
+</html>
---
+++ Links/SystemAdmin/Dashboard_SystemAdmin.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500&family=Raleway:ital,wght@0,300;1,100&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../Assets/css/SystemAdmin/Dashboard_SystemAdmin.css">
+    <title>Admin</title>
+</head>
+<body>
+    <main>
+        <section class="Glass">
+            <div class="Dashboard">
+                <div class="User">
+                    <img src="../../Assets/img/Avatar.svg" class="Profile">
+                    <h3>Name User</h3>
+                    <p>Position</p>
+                </div>
+                <div class="Links">
+                    <div class="Link"> 
+                        <img src="../../Assets/img/Icons/Home.svg" alt="">
+                        <a href=""><h2>Inicio</h2></a>
+                    </div>
+                    <div class="Link"> 
+                        <img src="../../Assets/img/Icons/Inventories.svg" alt="">
+                        <a href=""><h2>Inventarios</h2></a>
+                    </div>
+                    <div class="Link"> 
+                        <img src="../../Assets/img/Icons/Loans.svg" alt="">
+                        <a href=""><h2>Préstamos</h2></a>
+                    </div>
+                    <div class="Link"> 
+                        <img src="../../Assets/img/Icons/Person-add.svg" alt="">
+                        <a href="CreateUsers.php"><h2>Crear Usuarios</h2></a>
+                    </div>
+                    <div class="Link"> 
+                        <img src="../../Assets/img/Icons/Profile.svg" alt="">
+                        <a href=""><h2>Perfil</h2></a>
+                    </div> 
+                    <div class="Link"> 
+                        <img src="../../Assets/img/Icons/Notifications.svg" alt="">
+                        <a href=""><h2>Notificaciones</h2></a>
+                    </div>
+                    <div class="Link"> 
+                        <img src="../../Assets/img/Icons/Settings.svg" alt="">
+                        <a href=""><h2>Configuración</h2></a>
+                    </div>
+                    <div class="Link"> 
+                        <img src="../../Assets/img/Icons/Exit.svg" alt="">
+                        <a href="../../Index.php"><h2>Cerrar sesión</h2></a>
+                    </div>
+                </div>
+            </div>   
+            <div class="Search-box">
+                <input type="text" placeholder="Toca para escribir">
+                    <img src="../../Assets/img/Icons/Search.svg" class="Search"> 
+            </div>   
+        </section>   
+    </main>
+</body>
+</html>
