0fd1a38e7fb1e6bf7e19dad2949ea50919cbd4cd
EstefaCa <83930607+EstefaCa@users.noreply.github.com>
2021-10-27 13:24:28 -0500

    Se elimina todo

--- For Now, no/Forms/Connection/Connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Conexión de base de datos.
- * @author Estefanía <estefaniaalarcon2011@gmail.com>
- * @version 1.0
- * @return string configuración de la conexión
- */
-    $conexion_bd=new PDO('mysql:host=localhost;dbname=zaiko_db','root','');
-?>
--- README.md
+++
@@ -1,2 +0,0 @@
-# Project-Zaiko
-Zaiko, inventory manager, is a web application, developed by 4 students. This application will be used by the Mayor's Office of the municipality of Abejorral.
